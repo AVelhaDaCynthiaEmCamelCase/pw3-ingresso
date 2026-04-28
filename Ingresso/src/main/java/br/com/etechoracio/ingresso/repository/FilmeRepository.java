b2d52ef7d77ed97667c9fa167922014906779903
Gustavoh345 <felipesardinham@gmail.com>
2026-04-28 15:24:57 -0300

    primeiro commit da velhaDaCyntia

--- Ingresso/src/main/java/br/com/etechoracio/ingresso/repository/FilmeRepository.java
+++ Ingresso/src/main/java/br/com/etechoracio/ingresso/repository/FilmeRepository.java
@@ -1,4 +1,6 @@
 package br.com.etechoracio.ingresso.repository;
 
-public interface FilmeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FilmeRepository extends JpaRepository<Filme, Long> {
 }
